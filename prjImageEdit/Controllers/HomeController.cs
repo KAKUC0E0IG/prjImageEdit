62676cdf862e3ef51d2812f45e4726226760c4e8
語喬 郭 <kakuc0e0ig@gmail.com>
2024-03-13 11:15:56 +0800

    排版、增加文字框輸入與移動功能的js

--- prjImageEdit/Controllers/HomeController.cs
+++ prjImageEdit/Controllers/HomeController.cs
@@ -23,10 +23,16 @@ namespace prjImageEdit.Controllers
             return View();
         }
 
+        public IActionResult figmatest()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
--- prjImageEdit/wwwroot/js/site.js
+++ prjImageEdit/wwwroot/js/site.js
@@ -1,4 +1,70 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//預設圖片為第一張
+const firstsrc = document.getElementById("1").getAttribute("src")
+$("#mainimage").attr("src", firstsrc)
 
-// Write your JavaScript code.
+//點擊小圖更換背景
+function imagechange(id) {
+    console.log("click" + id)
+    const src = document.getElementById(id).getAttribute("src")
+    $("#mainimage").attr("src", src)
+}
+
+//文字框移動
+let mouseX = 0;
+let mouseY = 0;
+let offsetX = 0;
+let offsetY = 0;
+let isDown = false;
+var textmove = document.querySelector('#textmove')
+var div = document.querySelector(".mydiv");
+
+//手機觸控
+div.addEventListener('touchstart', function (e) {
+    isDown = true
+    mouseX = e.targetTouches[0].pageX
+    mouseY = e.targetTouches[0].pageY
+    document.addEventListener("touchmove",movetouch)
+})
+
+div.addEventListener('touchend', function (e) {
+    if (isDown)
+    {
+        offsetX += e.targetTouches[0].pageX - mouseX
+        offsetY += e.targetTouches[0].pageY - mouseY
+    }
+    isDown = false
+    document.addEventListener("touchmove", movetouch)
+})
+
+function movetouch(){
+    if (isDown) {
+        const dx = e.targetTouches[0].pageX - mouseX
+        const dy = e.targetTouches[0].pageY - mouseY
+        div.style.transform = `translate(${offsetX + dx}px, ${offsetY + dy}px)`
+    }
+}
+
+//電腦滑鼠
+div.addEventListener('mousedown', function (e) {
+    isDown = true
+    mouseX = e.pageX
+    mouseY = e.pageY
+    document.addEventListener("mousemove", move)
+})
+
+div.addEventListener('mouseup', function (e) {
+    if (isDown) {
+        offsetX += e.pageX - mouseX
+        offsetY += e.pageY - mouseY
+    }
+    isDown = false
+    document.addEventListener("mousemove", move)
+})
+
+function move() {
+    if (isDown) {
+        const dx = e.pageX - mouseX
+        const dy = e.pageY - mouseY
+        div.style.transform = `translate(${offsetX + dx}px, ${offsetY + dy}px)`
+    }
+}
